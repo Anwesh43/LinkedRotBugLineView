20e88bbc383d02cd0ace72f215578c5f2aa6e528
Anwesh43 <anweshthecool0@gmail.com>
2019-02-28 02:55:53 +0530

    adding drawRBLNode canvas extension method

--- rotbuglineview/src/main/java/com/anwesh/uiprojects/rotbuglineview/RotBugLineView.kt
+++ rotbuglineview/src/main/java/com/anwesh/uiprojects/rotbuglineview/RotBugLineView.kt
@@ -26,4 +26,36 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sf() : Float = 1f - 2 * (this % 2)
+
+fun Canvas.drawRBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val yGap : Float = (2 * size) / (lines + 1)
+    val r : Float = yGap / sizeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2 * i.sf())
+    save()
+    translate(0f, -size + yGap)
+    for (j in 0..(lines - 1)) {
+        save()
+        translate(0f, j * yGap)
+        drawCircle(0f, 0f, r, paint)
+        save()
+        rotate(30f * sc1.divideScale(j, lines) * j.sf())
+        drawLine(0f, 0f, size, 0f, paint)
+        restore()
+        restore()
+    }
+    restore()
+    restore()
+}
